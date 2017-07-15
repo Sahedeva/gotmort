2b1643a3d79ea0f8d92f19a27d51db7d27a82215
Robert Weilbaecher <robertgweilbaecher@gmail.com>
2017-07-15 14:46:30 -0500

    forms

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   history: Array,
-  currentBet: Object,
+  currentBet: Array,
   created_at: Date,
   updated_at: Date
 });
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,46 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Game of Thrones Mortality' });
 });
 
+router.get('/characterForm', function(req,res,next){
+  res.render('characterForm', {title: 'Character Form'});
+});
+
+router.post('/characterForm', function(req,res,next){
+  var name = req.body.name;
+  var newCharacter = Character({
+      name:name,
+      isAlive: true
+  });
+});
+
+router.get('/userForm', function(req,res,next){
+
+  Character.find({}, function(err, characters){
+      console.log(characters);
+      var len = characters.length
+      res.render('userForm', {title: 'User Form', characters:characters, len:len});
+    });
+
+});
+
+  router.post('/submitUser', function(req,res,next){
+    var name = req.body.name;
+    console.log(req.body);
+    var currentBet = []
+    for (var i = 0;i<5;i++) {
+      currentBet[i] = req.body['characterChoice'+(i+1)];
+    }
+    var newUser = User({
+        name:name,
+        currentBet:currentBet
+    });
+
+  // Save the user
+  newUser.save(function(err) {
+      if (err) console.log(err);
+
+      res.redirect('/userForm');
+  });
+});
+
 module.exports = router;
